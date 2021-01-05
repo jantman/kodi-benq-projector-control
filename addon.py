423ccc4947876d298a5a8605f0e9ffcbb1e65014
Jason Antman <jason@jasonantman.com>
2021-01-05 18:25:51 -0500

    fix last commit

--- addon.py
+++ addon.py
@@ -34,5 +34,5 @@ mon = xbmc.Monitor()
 ScreenSaverWatcher()
 while not mon.abortRequested():
         if mon.waitForAbort(10):
+            xbmc.log("ScreenSaverWatcher is exiting at %s" % time.time(), level=xbmc.LOGNOTICE)
             break
-        xbmc.log("Addon is exiting %s" % time.time(), level=xbmc.LOGNOTICE)

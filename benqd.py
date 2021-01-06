17cd3770714a7a2bf12b5304517fb3791d90f4d5
Jason Antman <jason@jasonantman.com>
2021-01-06 07:49:28 -0500

    initial code to control projector

--- benqd.py
+++ benqd.py
@@ -2,14 +2,28 @@
 """
 BenQ RS-232 projector control daemon
 
+Configuration is via environment variables:
+
+FLASK_DEBUG - set to 'true' to enable Flask debugging
+FLASK_PORT - defaults to 80
+PROJECTOR_DEVICE - defaults to '/dev/ttyUSB0'
+SERIAL_BAUDRATE - defaults to 115200
+
+The API is as follows:
+
+GET / - return JSON including "power_on" boolean
+POST / - POST JSON, including one of:
+  {"power_on": true|false} - turn the projector on or off
+
 AGPL 3.0
 https://github.com/jantman/kodi-benq-projector-control
 """
 
 import ctypes
-import stuct
-import pyserial
+import struct
+import serial
 import logging
+import os
 
 from flask import Flask, request, jsonify
 
@@ -22,6 +36,65 @@ app = Flask(__name__)
 app.logger.propagate = True
 
 
+class ProjectorCommunicator:
+
+    def __init__(self):
+        dev = os.environ.get('PROJECTOR_DEVICE', '/dev/ttyUSB0')
+        baud = int(os.environ.get('SERIAL_BAUDRATE', '115200'))
+        app.logger.info(
+            'Opening serial connection to %s at %s', dev, baud
+        )
+        try:
+            self.conn = serial.Serial(dev, baud, timeout=0.5)
+        except Exception as ex:
+            app.logger.exception(
+                'Unable to open serial connection to %s at %s: %s',
+                dev, baud, ex, exc_info=True
+            )
+            raise
+        app.logger.info('ProjectorCommunicator initialized.')
+
+    @property
+    def is_on(self):
+        result = self._send_command(b'\r*pow=?#\r').strip()
+        if result == '*POW=ON#':
+            return True
+        if result == '*POW=OFF#':
+            return False
+        raise RuntimeError('ERROR: Unknown power query response: "%s"' % result)
+
+    def turn_on(self):
+        result = self._send_command(b'\r*pow=on#\r').strip()
+        if result == '*POW=ON#':
+            return True
+        raise RuntimeError(
+            'ERROR: Unexpected response to pow=on: "%s"' % result
+        )
+
+    def turn_off(self):
+        result = self._send_command(b'\r*pow=off#\r').strip()
+        if result == '*POW=OFF#':
+            return True
+        raise RuntimeError(
+            'ERROR: Unexpected response to pow=off: "%s"' % result
+        )
+
+    def _send_command(self, cmd):
+        # returns a STRING
+        app.logger.info('Send command: %s', cmd)
+        self.conn.write(cmd)
+        # first line should echo the command back
+        buf = self.conn.readline()
+        app.logger.info('Readline: %s', buf)
+        # second line should be the response
+        buf = self.conn.readline()
+        app.logger.info('Readline: %s', buf)
+        return buf.decode()
+
+
+COMM = ProjectorCommunicator()
+
+
 def uptime():
     libc = ctypes.CDLL('libc.so.6')
     buf = ctypes.create_string_buffer(4096)
@@ -32,82 +105,69 @@ def uptime():
     return uptime
 
 
-def humantime(secs):
-    if secs > 31536000:
-        return '%s years' % round(secs / 31536000, 1)
-    if secs > 2592000:
-        return '%s months' % round(secs / 2592000, 1)
-    if secs > 86400:
-        return '%s days' % round(secs / 86400, 1)
-    if secs > 3600:
-        return '%s hours' % round(secs / 3600, 1)
-    if secs > 60:
-        return '%s minutes' % round(secs / 60, 1)
-    return '%s seconds' % secs
-
-
 def get_status():
     try:
-        info = DISPLAY.dpms_info()
-    except Exception as ex:
-        app.logger.error('Exception getting dpms_info', exc_info=True)
         return {
-            'success': False,
-            'is_on': False,
-            'state': 'unknown',
-            'exception': str(ex)
+            'power_on': COMM.is_on,
+            'success': True,
+            'message': ''
         }
-    state = 'unknown'
-    is_on = False
-    if info.power_level == dpms.DPMSModeOn:
-        state = 'on'
-        is_on = True
-    elif info.power_level in [dpms.DPMSModeOff, dpms.DPMSModeStandby, dpms.DPMSModeSuspend]:
-        state = 'off'
-        is_on = False
-    return {
-        'success': True,
-        'is_on': is_on,
-        'state': state
-    }
-
-
-def ensure_timeouts():
-    timeouts = DISPLAY.dpms_get_timeouts()
-    app.logger.info('Current timeouts: %s', timeouts)
-    DISPLAY.dpms_set_timeouts(
-        DISPLAY_TIMEOUT_SEC, DISPLAY_TIMEOUT_SEC, DISPLAY_TIMEOUT_SEC
-    )
-    DISPLAY.sync()
-    app.logger.info('Timeouts updated to: %s seconds', DISPLAY_TIMEOUT_SEC)
+    except Exception as ex:
+        app.logger.exception(
+            'Exception getting power state: %s', ex, exc_info=True
+        )
+        return {
+            'power_on': None,
+            'success': False,
+            'message': 'Exception getting power state: %s' % ex
+        }
 
 
 @app.route('/', methods=['GET', 'POST'])
 def handle():
     app.logger.info('Got request with values: %s' % dict(request.form))
-    if request.method == 'POST':
-        data = request.get_json()
-        app.logger.info('Got POST from %s: %s', request.remote_addr, data)
-        status = get_status()
-        if data.get('state') is True and status['is_on'] is False:
-            app.logger.info('Turning on screen via DPMS')
-            DISPLAY.dpms_force_level(dpms.DPMSModeOn)
-            DISPLAY.sync()
-        elif data.get('state') is False and status['is_on'] is True:
-            app.logger.info('Turning off screen via DPMS')
-            DISPLAY.dpms_force_level(dpms.DPMSModeOff)
-            DISPLAY.sync()
-        else:
-            app.logger.info('POST but no state change')
     status = get_status()
     status['uptime'] = uptime()
-    status['server'] = 'dpmsweb.py managed by privatepuppet::couchpi'
-    return jsonify(status)
+    status['server'] = 'benqd.py from https://github.com/jantman/' \
+                       'kodi-benq-projector-control'
+    if not status['success']:
+        return jsonify(status), 500
+    if request.method != 'POST':
+        return jsonify(status)
+    # else POST request
+    data = request.get_json()
+    app.logger.info('Got POST from %s: %s', request.remote_addr, data)
+    rcode = 200
+    if data.get('power_on') is True and status['power_on'] is False:
+        app.logger.info('Turning projector on')
+        try:
+            COMM.turn_on()
+            status['power_on'] = True
+            status['message'] = 'Projector powered on'
+            rcode = 201
+        except Exception as ex:
+            status['success'] = False
+            status['message'] = 'Exception turning on projector: %s' % ex
+            rcode = 500
+    elif data.get('power_on') is False and status['power_on'] is True:
+        app.logger.info('Turning projector off')
+        try:
+            COMM.turn_off()
+            status['power_on'] = False
+            status['message'] = 'Projector powered off'
+            rcode = 201
+        except Exception as ex:
+            status['success'] = False
+            status['message'] = 'Exception turning off projector: %s' % ex
+            rcode = 500
+    else:
+        app.logger.info('POST but no state change')
+        status['message'] = 'Projector already in desired state.'
+    return jsonify(status), rcode
 
 
 if __name__ == '__main__':
-    debug = 'FLASK_DEBUG' in os.environ
-    ensure_timeouts()
+    debug = os.environ.get('FLASK_DEBUG', '') == 'true'
     app.run(
         host="0.0.0.0", port=int(os.environ.get('FLASK_PORT', '80')),
         debug=debug
